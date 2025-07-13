1b95054d13031caecd7bd44d206bed4aa643c864
levkopo <levkopoag@gmail.com>
2025-07-13 21:06:03 +0500

    fix: опечатка

--- src/pages/MoviesPage.tsx
+++ src/pages/MoviesPage.tsx
@@ -9,7 +9,7 @@ import {MoviesView} from "../components";
 export const MoviesPage = observer(() => {
     return <SplitCol width="100%" maxWidth="1200px" stretchedOnMobile autoSpaced>
         <PanelHeader>
-            Каталок фильмов
+            Каталог фильмов
         </PanelHeader>
 
         <Group>
